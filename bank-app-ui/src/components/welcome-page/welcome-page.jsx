0de993ae54267e25f410ad4473e59b662e50efd2
Catalin <cata_banu98@yahoo.com>
2021-09-23 21:28:38 +0300

    Clean up and create welcome-page directory

---
+++ bank-app-ui/src/components/welcome-page/welcome-page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './welcome-page.css'
+
+function WelcomePage(){
+    return(
+        <div className="title">
+            <h1>Welcome from welcome page</h1>
+        </div>
+    )
+}
+
+export default WelcomePage
