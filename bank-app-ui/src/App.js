5008422c905bcb30f810c922287dc43319b5e164
Rares <runceanrares@yahoo.com>
2021-09-29 22:29:35 +0300

    update routing and design

--- bank-app-ui/src/App.js
+++ bank-app-ui/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import WelcomePage from './components/welcome-page/welcome-page';
 import LoginPage from './components/login-page/login-page';
+import RegisterPage from './components/register-page/register-page';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={WelcomePage}/>
           <Route path='/login' component={LoginPage}/>
+          <Route path='/register' component={RegisterPage}/>
         </Switch>
       </div>
     </Router>
--- bank-app-ui/src/components/welcome-page/welcome-page.jsx
+++ bank-app-ui/src/components/welcome-page/welcome-page.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import './welcome-page.css'
-import logo from "../../resources/logo.png"
+import logo from "../../resources/logo-v1.png"
 
 function WelcomePage(){
     return(
@@ -9,14 +9,15 @@ function WelcomePage(){
             <div className="welcome-page-container">
               <div className="logo-container">
                 <img className="image"  src={logo} alt="Not found"/>
-                <p className="image-text" >The image is your gaze</p>
             </div>
             <div className="welcome-buttons-area">
                 <p className="welcome-text">   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <Link to='/login'>
                     <button className="buttons">Login</button>
                 </Link>
-                <button className="buttons">Register</button>
+                <Link to='/register'>
+                    <button className="buttons">Register</button>
+                </Link>
             </div>
            </div>
         </div>
