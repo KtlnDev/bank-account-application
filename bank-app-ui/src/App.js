d51d1f9669d507e86b72c53bb8251c2b29cad187
Catalin <cata_banu98@yahoo.com>
2021-09-29 21:55:42 +0300

    Create routing and design buttons from welcome page

--- bank-app-ui/src/App.js
+++ bank-app-ui/src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import WelcomePage from './components/welcome-page/welcome-page';
+import LoginPage from './components/login-page/login-page';
 
 function App() {
   return (
-    <div className="App">
-      <WelcomePage></WelcomePage>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path='/' exact component={WelcomePage}/>
+          <Route path='/login' component={LoginPage}/>
+        </Switch>
+      </div>
+    </Router>
+    
   );
 }
 
--- bank-app-ui/src/components/welcome-page/welcome-page.jsx
+++ bank-app-ui/src/components/welcome-page/welcome-page.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import {Link} from "react-router-dom"
 import './welcome-page.css'
 import logo from "../../resources/logo.png"
 
@@ -12,8 +13,10 @@ function WelcomePage(){
             </div>
             <div className="welcome-buttons-area">
                 <p className="welcome-text">   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <button className="button-login">Login</button>
-                <button className="button-register">Register</button>
+                <Link to='/login'>
+                    <button className="buttons">Login</button>
+                </Link>
+                <button className="buttons">Register</button>
             </div>
            </div>
         </div>
