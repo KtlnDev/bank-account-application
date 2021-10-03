c1dbaa6ef63d4526de509eaed1ffa1a2a5013c02
Rares <runceanrares@yahoo.com>
2021-10-03 20:23:42 +0300

    create input for login and register page

--- bank-app-ui/src/components/login-page/login-page.jsx
+++ bank-app-ui/src/components/login-page/login-page.jsx
@@ -1,14 +1,25 @@
 import React from "react"
 import './login-page.css'
 import logo from "../../resources/logo.png"
+import { useForm } from "react-hook-form";
 
 function LoginPage(){
+    const { register, handleSubmit, watch, errors } = useForm();
+    const onSubmit = data => console.log(data);
     return(
     <div className="page-container">
         <div className="box-container">
             <img className="logo"  src={logo} alt="Not found"/>
             <div className="welcome-buttons-area">
                 LOGIN PAGE
+            <br/><br/>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                 <input className="form-container" type="email" name="email" placeholder="Email"  />
+                 <br/><br/>
+                 <input className="form-container" type="text" name="password" placeholder="Password"/>
+                 <br/><br/>
+                <input className="buttons" type="submit" value="Submit" />
+            </form>
             </div>
         </div>
     </div>
--- bank-app-ui/src/components/register-page/register-page.jsx
+++ bank-app-ui/src/components/register-page/register-page.jsx
@@ -9,6 +9,23 @@ function RegisterPage(){
             <img className="logo"  src={logo} alt="Not found"/>
             <div className="welcome-buttons-area">
                 REGISTER PAGE
+                <br/><br/>
+                <form>
+                    <input className="form-container" type="email" name="email" placeholder="Email" required />
+                    <br />
+                    <br />
+                    <input className="form-container" type="text" name="firstname" placeholder="First Name" required />
+                    <br />
+                    <br />
+                    <input className="form-container" type="text" name="lastname" placeholder="Last Name" required />
+                    <br />
+                    <br />
+                    <input className="form-container" type="text" name="lastname" placeholder="Phone Number" required />
+                    <br />
+                    <br />
+                    <input className="buttons" type="submit" value="Submit" />
+                </form>
+              
             </div>
         </div>
     </div>
