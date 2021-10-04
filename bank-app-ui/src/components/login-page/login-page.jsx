4ce6166fd7a89d5c03460148de15f07fcf3c7f4a
Catalin <cata_banu98@yahoo.com>
2021-10-04 21:09:32 +0300

    Refactor login page

--- bank-app-ui/src/components/login-page/login-page.jsx
+++ bank-app-ui/src/components/login-page/login-page.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import './login-page.css'
 import logo from "../../resources/logo.png"
+import LoginIcon from "../../resources/login-icon.png"
 import { useForm } from "react-hook-form";
 
 function LoginPage(){
@@ -8,17 +9,16 @@ function LoginPage(){
     const onSubmit = data => console.log(data);
     return(
     <div className="page-container">
-        <div className="box-container">
+        <div className="box-container-login">
             <img className="logo"  src={logo} alt="Not found"/>
-            <div className="welcome-buttons-area">
-                LOGIN PAGE
-            <br/><br/>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                 <input className="form-container" type="email" name="email" placeholder="Email"  />
-                 <br/><br/>
-                 <input className="form-container" type="text" name="password" placeholder="Password"/>
-                 <br/><br/>
-                <input className="buttons" type="submit" value="Submit" />
+            <div className="login-container">
+                <img  src={LoginIcon} alt="Not found"/>
+                <form  className="login-form" >
+                    <label className="login-label">Email</label>
+                    <input className="input-field" type="email" name="email" placeholder="Enter your email address"  />
+                    <label className="login-label">Password</label>
+                    <input className="input-field" type="text" name="password" placeholder="Enter your password"/>
+                    <input className="login-button" type="submit" value="Login" />
             </form>
             </div>
         </div>
